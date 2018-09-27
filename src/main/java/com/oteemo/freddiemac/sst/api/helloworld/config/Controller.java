8973697b2b546be112cc07b6021a10c1b3602286
tdoteemo <tdave@oteemo.com>
2018-09-27 10:47:31 -0400

    FM SpringBoot POC api

---
+++ src/main/java/com/oteemo/freddiemac/sst/api/helloworld/config/Controller.java
@@ -0,0 +1,21 @@
+package com.oteemo.freddiemac.sst.api.helloworld.config;
+
+import com.oteemo.freddiemac.sst.api.helloworld.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class Controller {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/helloworld")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
